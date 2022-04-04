c7962fd930e49e746934d06df5dc8de84098e097
leo1p <leo1pacheco@hotmail.com>
2022-04-04 08:07:48 -0500

    Updates

---
+++ precache-manifest.2a1e81a5a0f9b7540adcb13a56ac8190.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "15e49d16619c779fb51b",
+    "url": "/pageLaguna/static/js/0.a644bc30.chunk.js"
+  },
+  {
+    "revision": "bdd6ee4e55c93f736a27",
+    "url": "/pageLaguna/static/css/main.936f76cc.chunk.css"
+  },
+  {
+    "revision": "bdd6ee4e55c93f736a27",
+    "url": "/pageLaguna/static/js/main.8addd2bf.chunk.js"
+  },
+  {
+    "revision": "c7f566060c5d09aca4ba",
+    "url": "/pageLaguna/static/js/runtime~main.9bed4888.js"
+  },
+  {
+    "revision": "caf12a4bb3487ccf11bb",
+    "url": "/pageLaguna/static/js/3.2f1945f8.chunk.js"
+  },
+  {
+    "revision": "2b524f2033d1a9123b70",
+    "url": "/pageLaguna/static/js/4.790b0e1d.chunk.js"
+  },
+  {
+    "revision": "f88409294a5aa6b850a4",
+    "url": "/pageLaguna/static/js/5.7f8b83f4.chunk.js"
+  },
+  {
+    "revision": "7da1de3f5c92febe112e",
+    "url": "/pageLaguna/static/css/6.36378fac.chunk.css"
+  },
+  {
+    "revision": "7da1de3f5c92febe112e",
+    "url": "/pageLaguna/static/js/6.ca0f2c91.chunk.js"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/pageLaguna/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/pageLaguna/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/pageLaguna/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "/pageLaguna/static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "84c6fe1e4353c916d8f1452e82bdef8d",
+    "url": "/pageLaguna/static/media/materialdesignicons-webfont.84c6fe1e.woff2"
+  },
+  {
+    "revision": "1a95531b09ffc9b76104e0003b456a0e",
+    "url": "/pageLaguna/static/media/materialdesignicons-webfont.1a95531b.woff"
+  },
+  {
+    "revision": "6acc7d6cffa21f6eda4baaa90e835fd2",
+    "url": "/pageLaguna/static/media/materialdesignicons-webfont.6acc7d6c.ttf"
+  },
+  {
+    "revision": "be7691c114e7c9d0543b89e824c4003d",
+    "url": "/pageLaguna/static/media/materialdesignicons-webfont.be7691c1.eot"
+  },
+  {
+    "revision": "51b68d0604660b53c44400ccdef379c0",
+    "url": "/pageLaguna/static/media/materialdesignicons-webfont.51b68d06.svg"
+  },
+  {
+    "revision": "8b073cf702bddf835869b171fe03d73d",
+    "url": "/pageLaguna/static/media/img-1.8b073cf7.jpg"
+  },
+  {
+    "revision": "80e52295922c70fdaa79d54afe26e883",
+    "url": "/pageLaguna/static/media/bg-home.80e52295.jpg"
+  },
+  {
+    "revision": "d0b8f70643b398774a14ebe4e56bbdcb",
+    "url": "/pageLaguna/static/media/img-2.d0b8f706.jpg"
+  },
+  {
+    "revision": "07c9babb2e101fa952538a5cf85258c5",
+    "url": "/pageLaguna/static/media/logo.07c9babb.png"
+  },
+  {
+    "revision": "058968437a6816318c07446758d51df7",
+    "url": "/pageLaguna/static/media/img-1.05896843.jpg"
+  },
+  {
+    "revision": "b2926a4cba8de7a66bc6d3e07c0b4682",
+    "url": "/pageLaguna/static/media/img-2.b2926a4c.jpg"
+  },
+  {
+    "revision": "23d0cae639e952805c67d63803ee81c8",
+    "url": "/pageLaguna/static/media/img-3.23d0cae6.jpg"
+  },
+  {
+    "revision": "41b51894b16a8fa4cabb3190cd9efda0",
+    "url": "/pageLaguna/static/media/img-4.41b51894.jpg"
+  },
+  {
+    "revision": "62c6c4cbe6933a556d9e4393434cca8c",
+    "url": "/pageLaguna/static/media/img-5.62c6c4cb.jpg"
+  },
+  {
+    "revision": "4bd22c934462158e687e621989e88631",
+    "url": "/pageLaguna/static/media/img-6.4bd22c93.jpg"
+  },
+  {
+    "revision": "d0136eef8937b403c44d5d2b1f3431ca",
+    "url": "/pageLaguna/static/media/img-7.d0136eef.jpg"
+  },
+  {
+    "revision": "228be381fed4705505eba3b7870ceacc",
+    "url": "/pageLaguna/static/media/img-8.228be381.jpg"
+  },
+  {
+    "revision": "13ceea236921c801923f8b702653988a",
+    "url": "/pageLaguna/static/media/img-9.13ceea23.jpg"
+  },
+  {
+    "revision": "36fb4b0402f240268657f16ccbeb4283",
+    "url": "/pageLaguna/index.html"
+  }
+];
